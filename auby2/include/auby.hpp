5ce457a7cbce8877df2cf52ed555e3052f19940e
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-17 11:05:50 +0300

    Add files via upload

---
+++ auby2/include/auby.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <unordered_set>
+
+#include <cocos-ext.h>
+#include <cocos2d.h>
+using namespace cocos2d;
+using namespace cocos2d::extension;
+#include <fmod/placeholder.hh>
+#include <gd/ext.hh>
+#include <gd/gd.hh>
+
+#include "auby/auby.hpp"
+#include "auby/internal.hpp
+#include "auby/magic.hpp"
+#include "auby/settings.hpp"
