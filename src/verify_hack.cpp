c1c2c2efb1ba0b96554be1728bdeb9128de0193b
Gtxripmxt <gtxripmxt@gmail.com>
2025-05-04 15:57:19 +0300

    Update verify_hack.cpp

--- src/verify_hack.cpp
+++ src/verify_hack.cpp
@@ -1,5 +1,5 @@
 #include "auby2/src/settings.hpp"
-#include <auby.hpp>
+#include "auby2/include/auby/auby.hpp"
 
 namespace {
 bool $(EditLevelLayer::init)(EditLevelLayer* self, GJGameLevel* level) {
